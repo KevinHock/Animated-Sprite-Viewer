47eb932effacf444ec3ab04c332926d53db192be
Kevin <Kevin@172.24.205.152>
2012-09-18 18:23:30 -0400

    I'm inFodors class

---
+++ src/animated_sprite_viewer/events/JlistHandler.java
@@ -0,0 +1,24 @@
+package animated_sprite_viewer.events;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Kevin
+ */
+public class JlistHandler implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    //list handler
+    
+    //jlist tutorial handler listen
+}
